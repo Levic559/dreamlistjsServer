039570371f52f38fa3520d0283bdd6f2eeafcd84
Levic559 <ychen559@my.bcit.ca>
2022-04-11 14:44:14 -0700

    add the new serverurl to whiteList

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,7 +1,6 @@
 
 const whiteList = [
-    'http://echoserver-env.eba-bccqr6dt.us-east-1.elasticbeanstalk.com',
-    'http://dreamlist-env.eba-gh5misbe.us-west-2.elasticbeanstalk.com/',
+    'http://dreamlistserver-env.eba-pk9mn7ke.us-west-2.elasticbeanstalk.com/',
     'https://echo-sage.vercel.app',
     'http://127.0.0.1:8080',
     'http://localhost:3750',
