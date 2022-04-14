6b380052fc58fe395f3de1bf8811324b0d6cebab
Levi Chen <72251069+Levic559@users.noreply.github.com>
2022-04-14 13:33:19 -0700

    add a new whiteList

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,7 +1,6 @@
 
 const whiteList = [
-    'http://echoserver-env.eba-bccqr6dt.us-east-1.elasticbeanstalk.com',
-    'http://dreamlistserver-env-1.eba-rr2fbshw.us-west-2.elasticbeanstalk.com/',
+    'https://happy-cat.ca/',
     'https://echo-sage.vercel.app',
     'http://127.0.0.1:8080',
     'http://localhost:3750',
