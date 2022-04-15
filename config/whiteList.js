444b8ecee566361e27ed5f9ac0869b2112e945c1
Levic559 <ychen559@my.bcit.ca>
2022-04-14 20:52:11 -0700

    add a aws url list

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,8 +1,8 @@
 
 const whiteList = [
-    'https://happy-cat.ca',
     'https://dreamlist.vercel.app',
     'http://Dreamlistawsserver-env.eba-usrq3iz5.us-west-2.elasticbeanstalk.com',
+    'http://dreamlistaws-env.eba-sjkp9rf7.us-west-2.elasticbeanstalk.com/',
     'http://127.0.0.1:8080',
     'http://localhost:3750',
     'http://localhost:3000'
