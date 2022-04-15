a402d8cd3738565857e15902180941d338a8527e
Levic559 <ychen559@my.bcit.ca>
2022-04-14 23:31:29 -0700

    add verel url

--- config/whiteList.js
+++ config/whiteList.js
@@ -3,7 +3,7 @@ const whiteList = [
     'https://dreamlist.vercel.app',
     'http://Dreamlistawsserver-env.eba-usrq3iz5.us-west-2.elasticbeanstalk.com',
     'http://dreamlistaws-env.eba-sjkp9rf7.us-west-2.elasticbeanstalk.com',
-    'https://dreamlist-a3r4iiqud-levic559.vercel.app/',
+    'https://dreamlist-yvuskt9ac-levic559.vercel.app',
     'http://127.0.0.1:8080',
     'http://localhost:3750',
     'http://localhost:3000'
