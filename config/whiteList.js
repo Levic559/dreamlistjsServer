e72c1fed16b5d52619dcac3606e93b2118fb87e0
Levic559 <ychen559@my.bcit.ca>
2022-04-14 20:16:26 -0700

    add a new white list (aws)

--- config/whiteList.js
+++ config/whiteList.js
@@ -2,6 +2,7 @@
 const whiteList = [
     'https://happy-cat.ca',
     'https://dreamlist.vercel.app/',
+    'http://dreamlistserver-env-1.eba-rr2fbshw.us-west-2.elasticbeanstalk.com/',
     'http://127.0.0.1:8080',
     'http://localhost:3750',
     'http://localhost:3000'
